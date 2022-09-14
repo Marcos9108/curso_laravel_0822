bf1750373844f93eefae44589b9054f2951e81f7
Marcos Munoz <jmarcos9108@gmail.com>
2022-09-14 09:50:45 -0500

    EDIT FILE - Segundo cambio a INDEX DatoContacto Marcos

--- resources/views/DatoContacto/index.blade.php
+++ resources/views/DatoContacto/index.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 @section('content')
 
-    <div> <h3> INDEX de Datos de contacto</h3></div>
+    <div> <h3> INDEX de Datos de contacto Marcos</h3></div>
 
+    <div> <span> Tercer cambio Marcos</span></div>
+    <div> <span> Segundo cambio Marcos</span></div>
     <div> <span> Primer cambio Marcos</span></div>
 
 
